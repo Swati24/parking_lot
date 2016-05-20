c501ea58cd2cf6c6b84a735cf2459da3083f950b
Swati Aggarwal <swati.aggarwal2412@gmail.com>
2016-05-20 17:26:16 +0530

    rspec initialized

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ source "https://rubygems.org"
 gem 'activerecord', '4.2.6'
 gem 'mysql2'
 gem 'rake'
+gem 'rspec'
---
+++ spec/models/parking_spec.rb
@@ -0,0 +1,7 @@
+#require "rails_helper"
+
+RSpec.describe Parking, type: :model do
+  it "will create a new parking" do
+    
+  end
+end
